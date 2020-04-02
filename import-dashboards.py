4989dc522c5a3f78f505f42bcea1f01e852f30e7
adivinho <vadim.yalovets@percona.com>
2020-04-02 17:59:17 +0300

    PMM-5655 remove leftovers of Grafana plugins

--- import-dashboards.py
+++ import-dashboards.py
@@ -247,18 +247,21 @@ def add_panels():
 
 def rename_panels():
     for app in ['panels']:
-        print '  * Renaming %r' % (app,)
+        print ('  * Renaming ' + app)
         panels_list = os.listdir(GRAFANA_PLUGINS_DR)
         for panel in panels_list:
             panel_path = os.path.join(GRAFANA_PLUGINS_DR, panel, 'dist/plugin.json')
             if os.path.exists(panel_path):
-                print '   * %r -> ' % (panel,),
+                print ('   * ' + panel + ' -> '),
                 with open(panel_path, 'r') as f:
                     panel_params = json.loads(f.read())
-                    print '%r' % (panel_params['id'],)
-                    os.renames(os.path.join(GRAFANA_PLUGINS_DR, panel), os.path.join(GRAFANA_PLUGINS_DR, panel_params['id']))
-                    if os.path.exists(os.path.join(GRAFANA_PLUGINS_DR, panel)):
-                        os.rmdir(os.path.join(GRAFANA_PLUGINS_DR, panel))
+                    print (panel_params['id'])
+                    if not(os.path.exists(os.path.join(GRAFANA_PLUGINS_DR, panel_params['id']))):
+                        print ('    * Renaming panel')
+                        shutil.copytree(os.path.join(GRAFANA_PLUGINS_DR, panel), os.path.join(GRAFANA_PLUGINS_DR, panel_params['id']))
+                    if panel != panel_params['id']:
+                        print ('    * Cleaning up leftovers')
+                        shutil.rmtree(os.path.join(GRAFANA_PLUGINS_DR, panel))
 
 
 def copy_apps():
